881dbf42629fe0b26e5f4353b8108f4c31dc82b7
bmuratc <bmuratc@gmail.com>
2022-11-29 08:35:09 +0100

    init:
    - Performance analyze.
    - Use shared memory.

--- rgb2grayscale/rgb2grayscale.cu
+++ rgb2grayscale/rgb2grayscale.cu
@@ -8,10 +8,23 @@
 #include <unordered_map>
 #include <vector>
 
+#define gpuErrchk(ans) \
+    { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line) {
+    if (code != cudaSuccess) {
+        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
+                line);
+        exit(code);
+    }
+}
+
+__device__ int globalIdx_X() { return blockIdx.x * blockDim.x + threadIdx.x; }
+__device__ int globalIdx_Y() { return blockIdx.y * blockDim.y + threadIdx.y; }
+
 __global__ void rgba_to_grey(unsigned char *d_grey, uchar4 *d_rgba,
                              const std::size_t rows, const std::size_t cols) {
-    unsigned int idx = threadIdx.x + blockDim.x * blockIdx.x;
-    unsigned int idy = threadIdx.y + blockDim.y * blockIdx.y;
+    unsigned int idx = globalIdx_X();
+    unsigned int idy = globalIdx_Y();
 
     if (idx >= rows || idy >= cols) return;
 
@@ -35,18 +48,24 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    std::string in_img = std::string(argv[1]);
-    std::cout << "input image : " << in_img << std::endl;
-    std::string out_img = std::string(argv[2]);
-    std::cout << "output image : " << out_img << std::endl;
+    std::string in_img_name = std::string(argv[1]);
+    std::cout << "input image : " << in_img_name << std::endl;
+    std::string out_img_name = std::string(argv[2]);
+    std::cout << "output image : " << out_img_name << std::endl;
 
-    cv::Mat img = cv::imread(in_img);
+    cv::Mat img = cv::imread(in_img_name);
+    if (img.empty()) {
+        std::cout << "Error in reading image : " << in_img_name << std::endl;
+        return 1;
+    }
     cv::Mat img_rgba;
     cv::cvtColor(img, img_rgba, cv::COLOR_RGBA2BGRA);
-    std::size_t rows = img_rgba.rows;
-    std::size_t cols = img_rgba.cols;
 
-    cv::Mat img_grey(rows, cols, CV_8UC1);
+    std::size_t image_rows = img_rgba.rows;
+    std::size_t image_cols = img_rgba.cols;
+    const std::size_t image_dim_size = image_rows * image_cols;
+
+    cv::Mat img_grey(image_rows, image_cols, CV_8UC1);
 
     uchar4 *h_rgba = (uchar4 *)img_rgba.ptr<unsigned char>(0);
     unsigned char *h_grey = img_grey.ptr<unsigned char>(0);
@@ -54,39 +73,39 @@ int main(int argc, char *argv[]) {
     uchar4 *d_rgba;
     unsigned char *d_grey;
 
-    const unsigned int np = rows * cols;
-    cudaMalloc((void **)&d_rgba, sizeof(uchar4) * np);
-    cudaMalloc((void **)&d_grey, sizeof(unsigned char) * np);
-    cudaMemset((void **)d_grey, 0, sizeof(unsigned char) * np);
-    cudaMemcpy((void **)d_rgba, (void **)h_rgba, sizeof(uchar4) * np,
+    cudaMalloc((void **)&d_rgba, sizeof(uchar4) * image_dim_size);
+    cudaMalloc((void **)&d_grey, sizeof(unsigned char) * image_dim_size);
+    cudaMemset((void *)d_grey, 0, sizeof(unsigned char) * image_dim_size);
+    cudaMemcpy((void *)d_rgba, (void *)h_rgba, sizeof(uchar4) * image_dim_size,
                cudaMemcpyHostToDevice);
 
-    std::size_t BLOCK_WIDTH = 32;
-    std::cout << "BLOCK_WIDHT : " << BLOCK_WIDTH << std::endl;
+    const std::size_t BLOCK_WIDTH = 16;
+    const dim3 blockSize(BLOCK_WIDTH, BLOCK_WIDTH, 1);
+    unsigned int grid_x = (unsigned int)(image_rows / BLOCK_WIDTH);
+    unsigned int grid_y = (unsigned int)(image_cols / BLOCK_WIDTH);
+    const dim3 gridSize(grid_x, grid_y, 1);
+
     total_t = 0;
-    const dim3 block_size(BLOCK_WIDTH, BLOCK_WIDTH, 1);
-    unsigned int grid_x = (unsigned int)(rows / BLOCK_WIDTH + 1);
-    unsigned int grid_y = (unsigned int)(cols / BLOCK_WIDTH + 1);
-    const dim3 grid_size(grid_x, grid_y, 1);
-
     start_t = clock();
-    rgba_to_grey<<<grid_size, block_size>>>(d_grey, d_rgba, rows, cols);
+    rgba_to_grey<<<gridSize, blockSize>>>(d_grey, d_rgba, image_rows,
+                                          image_cols);
     end_t = clock();
-
     total_t += (double)(end_t - start_t) / CLOCKS_PER_SEC;
-
     printf("Total time GPU OP: %f\n", total_t / 10);
-    cudaMemcpy(h_grey, d_grey, sizeof(unsigned char) * np,
+
+    cudaMemcpy(h_grey, d_grey, sizeof(unsigned char) * image_dim_size,
                cudaMemcpyDeviceToHost);
 
-    cv::imwrite(out_img.c_str(), img_grey);
+    cv::imwrite(out_img_name.c_str(), img_grey);
 
-    // cv::imshow("rgb", img_rgba);
     // cv::imshow("grey", img_grey);
+    // cv::imshow("rgb", img);
     // cv::waitKey(0);
 
     cudaFree(d_rgba);
     cudaFree(d_grey);
+
+    gpuErrchk(cudaPeekAtLastError());
 
     return 0;
 }
